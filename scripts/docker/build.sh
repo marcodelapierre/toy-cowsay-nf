54505d69e4af872fe26a86f4a96811d20641513a
Marco De La Pierre <marco.delapierre@gmail.com>
2023-01-16 13:57:07 +0800

    updates to docker build scripts

--- scripts/docker/build.sh
+++ scripts/docker/build.sh
@@ -3,31 +3,15 @@
 # may need editing
 repo="quay.io/marcodelapierre/cowsay"
 date_tag="2023.01"
+archs="amd64 arm64"
 
 # should NOT need editing
-base="$( grep FROM Dockerfile | cut -d ' ' -f 2 )"
 manifest="${repo}:${date_tag}"
-archs="amd64 arm64-v8"
+plat="$( podman info | grep OsArch | cut -d ' ' -f 4 )"
+arch="$( echo $plat |cut -d / -f 2 )"
+suffix="-${plat/\//-}"
 
-for a in $archs ; do
-    podman pull \
-        --platform linux/${a/-/\/} \
-        ${base}
+image="${manifest}${suffix}"
+podman build -t ${image} .
+podman push ${image}
 
-    image="${manifest}-linux-${a}"
-
-    podman build \
-        --platform linux/${a/-/\/} \
-        --no-cache \
-        -t ${image} \
-        .
-
-    podman push ${image}
-done
-
-podman manifest create \
-    ${manifest} \
-    eval echo ${manifest}-linux-{${archs/ /,}}
-
-podman manifest push \
-    ${manifest}
