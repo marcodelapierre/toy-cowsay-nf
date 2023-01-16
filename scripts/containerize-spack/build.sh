e0100b945f64651eed532028315fc69df6d2b9a7
Marco De La Pierre <marco.delapierre@gmail.com>
2023-01-16 14:07:00 +0800

    adding build scripts for containerize-spack

---
+++ scripts/containerize-spack/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# may need editing
+repo="quay.io/marcodelapierre/cowsay"
+date_tag="2023.01"
+suffix="-spack"
+archs="amd64 arm64"
+
+# should NOT need editing
+manifest="${repo}:${date_tag}${suffix}"
+plat="$( podman info | grep OsArch | cut -d ' ' -f 4 )"
+arch="$( echo $plat |cut -d / -f 2 )"
+archsuffix="-${plat/\//-}"
+
+image="${manifest}${archsuffix}"
+podman build -t ${image} .
+podman push ${image}
---
+++ scripts/containerize-spack/manifest.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# may need editing
+repo="quay.io/marcodelapierre/cowsay"
+date_tag="2023.01"
+suffix="-spack"
+archs="amd64 arm64"
+
+# should NOT need editing
+manifest="${repo}:${date_tag}${suffix}"
+
+podman manifest create \
+    ${manifest} \
+    eval echo ${manifest}-linux-{${archs/ /,}}
+
+podman manifest push ${manifest} docker://${manifest}
